e4730e402f53077bab4865d25b2455c7ccebad9c
Lawrence-The-Cinarian <lawrencethecinarian@gmail.com>
2026-09-23 05:03:08 +0100

    update datal.c

--- src/datal.c
+++ src/datal.c
@@ -53,7 +53,7 @@ int readAndPrintStock(stockMarket *replace)
     return 1;
   }
   
-  while(fscanf(outputinfo, "%29s %9s %39s %8s %lf", replace->stockName, replace->stockSymbol, replace->date, replace->timeStamp, &replace->price) == 5)
+  while(fscanf(outputinfo, "Stock name: %29s\nStock symbol: %9s\nDate: %39s\nTime: %8s\nPrice: %lf\n\n", replace->stockName, replace->stockSymbol, replace->date, replace->timeStamp, &replace->price) == 5)
   {
   printf("Stock name: %s\nStock symbol: %s\nDate: %s\nTime: %s\nPrice: %.2f\n\n", replace->stockName, replace->stockSymbol, replace->date, replace->timeStamp, replace->price);
   }
